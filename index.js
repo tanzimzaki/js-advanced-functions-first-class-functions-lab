3e0825a512dbd2924ce87fdda317d5a73a2cd8f4
Aaron Byrom <aaronbyrom@protonmail.com>
2021-10-18 08:59:26 +1100

    my attempt

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const returnFirstTwoDrivers = function (drivers) {
-    return drivers.slice(0, 2);
+  return drivers.slice(0, 2);
 };
 
 const returnLastTwoDrivers = function (drivers) {
-    return drivers.slice(-2);
+  return drivers.slice(-2);
 };
 
-const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-function createFareMultiplier(num){
-      return function (number){
-         return number * num
-      }
+function createFareMultiplier(num) {
+  return function (number) {
+    return number * num;
+  };
 }
 
-function fetchSpecifiedDrivers(array, func){
-    return func(array)
+function fetchSpecifiedDrivers(array, func) {
+  return func(array);
 }
 
-function fareTripler(num){
-    return num * 3
-}
+const fareDoubler = createFareMultiplier(2);
 
-function fareDoubler(num){
-    return num * 2
-}
+const fareTripler = createFareMultiplier(3);
 
+function selectDifferentDrivers(drivers, returnedDrivers) {
+  return returnedDrivers(drivers);
+}
